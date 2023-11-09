99bc1b095e4e5b0702e1f73cf2eddff6a05b00ab
bpeters3 <97409612+bpeters3@users.noreply.github.com>
2023-11-08 22:16:12 -0500

    Main clean up and VS integration
    - Moved Command Line functions to Funcs.py
    - Added Visual Studio dev environment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import funcs
 import math
-from datetime import datetime
+import os
+clear = lambda: os.system('cls') #on Windows System
+
 
 groups, response = funcs.returnRequest()
 
@@ -10,33 +12,24 @@ group_choice = ''
 page = 1
 
 while group_choice == '':
-    print("\n           Showing page " + str(page) + " of " + str(math.ceil(groups_num/10)))
-    print("--------------------------------------------")
     
-    if groups_num - ((page-1)*10) < 10:
-        for i in range(0,groups_num - ((page-1)*10)):
-            print("[" + str(i) + "] : " + groups[i+(page-1)*10]['name'])
-    else:
-        for i in range(0,10):
-            print("[" + str(i) + "] : " + groups[i+(page-1)*10]['name'])
-            
-    print("--------------------------------------------")
-    print("    [P]revious page    |    [N]ext page")
+    funcs.printGroups(groups_num,page,groups)
+
     group_choice = input("Select a group to download all messages:")
     if group_choice == '':
-        print("\n"*100)
+        clear()
     elif group_choice[0].isnumeric():
         group_id = response[page-1][int(group_choice)]["id"] # First index indicates page, second indicates group. E.G: response[Page 0][Group 0]
         print("Selected Group: " + response[page-1][int(group_choice)]['name'])
     elif group_choice[0].lower() == 'p':
-        print("\n"*100)
+        clear()
         if page == 1:
             print("Error: Reached end of list")
         else:
             page -= 1
         group_choice = ''
     elif group_choice[0].lower() == 'n':
-        print("\n"*100)
+        clear()
         if page == math.ceil(groups_num/10):
             print("Error: Reached end of list")
         else:
@@ -45,27 +38,6 @@ while group_choice == '':
     else:
         group_choice = ''
 
-group_response = funcs.returnGroup(group_id)
-print("Retrieving messages from GroupMe API......")
-message_all = funcs.returnAllMessages(group_id)
-
-
-group_name = group_response["name"]
-invalid = '/\:*?\"<>|'
-for char in invalid:
-    group_name = group_name.replace(char,'') # Clean file name of illegal windows chars.
-
-message_output = open(group_name + '.txt','wb')
-
-print("Writing to " + group_name + ".txt ...")
-
-for i in reversed(message_all):
-    date_i = datetime.fromtimestamp(i["created_at"])
-    sender_i = i["name"]
-    message_i = i["text"]
-    line_out = str(date_i) + "[" + str(sender_i) + "] : " + str(message_i) + '\n'
-    message_output.write(line_out.encode('utf8'))
-
-message_output.close()
+funcs.groupWrite(group_id)
 
 print("File successfully written!")
